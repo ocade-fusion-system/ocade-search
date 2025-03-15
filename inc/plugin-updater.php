83b990015997e69a03014fc508cf394f0e1b28ee
Valentin Charrier <valentin.charrier@hotmail.fr>
2025-03-15 23:35:47 +0100

    Correction probleùme de mise a jour

--- inc/plugin-updater.php
+++ inc/plugin-updater.php
@@ -2,49 +2,58 @@
 // Sécurité : empêcher l'accès direct
 if (!defined('ABSPATH')) exit;
 
-
 add_filter('site_transient_update_plugins', function ($transient) {
   if (!is_object($transient)) $transient = new stdClass();
 
-  // Charger la fonction pour obtenir les infos du plugin
-  if (!function_exists('get_plugin_data')) {
-    require_once ABSPATH . 'wp-admin/includes/plugin.php';
-  }
+  // Définition dynamique du plugin
+  $organisation_github = 'ocade-fusion-system'; // ORGANISATION_GITHUB
+  $plugin_slug = 'ocade-blocks'; // DEPOT_GITHUB
+  $plugin_file = WP_PLUGIN_DIR . '/' . $plugin_slug . '/' . $plugin_slug . '.php';
+  $version_url = 'https://raw.githubusercontent.com/' . $organisation_github . '/' . $plugin_slug . '/master/version.txt';
+  $repo_url = 'https://github.com/' . $organisation_github . '/' . $plugin_slug;
+  $zip_url = $repo_url . '/releases/latest/download/' . $plugin_slug . '.zip';
 
-  $plugin_data = get_plugin_data(WP_PLUGIN_DIR . '/' . OCADE_PLUGIN_SLUG . '/' . OCADE_PLUGIN_SLUG . '.php');
+  // Icônes dynamiques
+  $icon_base_url = 'https://raw.githubusercontent.com/' . $organisation_github . '/' . $plugin_slug . '/master/assets/icons/';
+  $icons = [
+    'svg'  => $icon_base_url . 'icon.svg',
+    '1x'   => $icon_base_url . 'icon-1x.png',
+    '2x'   => $icon_base_url . 'icon-2x.png',
+    '3x'   => $icon_base_url . 'icon-3x.png',
+    '4x'   => $icon_base_url . 'icon-4x.png',
+    '5x'   => $icon_base_url . 'icon-5x.png',
+  ];
+
+  // Charger la fonction pour obtenir les infos du plugin
+  if (!function_exists('get_plugin_data')) require_once ABSPATH . 'wp-admin/includes/plugin.php';
+  if (!file_exists($plugin_file)) return $transient; // Sécurité : si le fichier du plugin n'existe pas
+
+  $plugin_data = get_plugin_data($plugin_file);
   $current_version = $plugin_data['Version'];
 
-  // Récupérer la version distante (version.txt)
-  $remote_version = get_transient(OCADE_REMOTE_VERSION);
-  if (false === $remote_version or NULL == $remote_version) {
-    $response = wp_remote_get(OCADE_VERSION_URL);
+  // Récupérer la version distante (mise en cache pour éviter les requêtes répétées)
+  $remote_version = get_transient($plugin_slug . '_remote_version');
+  if (false === $remote_version || is_null($remote_version)) {
+    $response = wp_remote_get($version_url);
     if (is_wp_error($response)) return $transient;
 
     $remote_version = trim(wp_remote_retrieve_body($response));
-    $remote_version = preg_replace('/[^0-9.]/', '', $remote_version);
-
-    set_transient(OCADE_REMOTE_VERSION, $remote_version, 6 * HOUR_IN_SECONDS);
+    $remote_version = preg_replace('/[^0-9.]/', '', $remote_version); // Nettoyage
+    $remote_version = trim($remote_version);
+    set_transient($plugin_slug . '_remote_version', $remote_version, 6 * HOUR_IN_SECONDS);
   }
 
   // Comparaison des versions
   if (version_compare($remote_version, $current_version, '>')) {
-    if (!isset($transient->response)) $transient->response = []; // Assurez-vous que c'est un tableau
+    $plugin_basename = plugin_basename($plugin_file);
 
-    $plugin_slug = plugin_basename(WP_PLUGIN_DIR . '/' . OCADE_PLUGIN_SLUG . '/' . OCADE_PLUGIN_SLUG . '.php');
-
-    $transient->response[$plugin_slug] = [
-      'plugin'       => $plugin_slug,
+    $transient->response[$plugin_basename] = (object) [
+      'slug'        => $plugin_slug,
+      'plugin'      => $plugin_basename,
       'new_version' => $remote_version,
-      'url'         => OCADE_PLUGIN_REPO,
-      'package'     => OCADE_ZIP_URL,
-      'icons'       => [
-        'svg'  => OCADE_ICON_SVG_URL,    // URL vers une icône SVG
-        '1x'   => OCADE_ICON_1X_URL,     // URL pour une image standard (150x150 px recommandé)
-        '2x'   => OCADE_ICON_2X_URL,     // URL pour les écrans haute résolution
-        '3x'   => OCADE_ICON_3X_URL,     // URL pour les écrans haute résolution
-        '4x'   => OCADE_ICON_4X_URL,     // URL pour les écrans haute résolution
-        '5x'   => OCADE_ICON_5X_URL,     // URL pour les écrans haute résolution
-      ],
+      'url'         => $repo_url,
+      'package'     => $zip_url,
+      'icons'       => $icons,
     ];
   }
 
@@ -53,6 +62,6 @@ add_filter('site_transient_update_plugins', function ($transient) {
 
 add_action('upgrader_process_complete', function ($upgrader_object, $options) {
   if ($options['action'] === 'update' && $options['type'] === 'plugin') {
-    delete_transient(OCADE_REMOTE_VERSION);
+    delete_transient('ocade-blocks_remote_version'); // Supprime le cache de version après mise à jour
   }
 }, 10, 2);
--- ocade-blocks.php
+++ ocade-blocks.php
@@ -10,38 +10,7 @@
  * Version: 1.0.8
  */
 
-
-// Chargement du système de mise à jour
-define("ORGANISATION_GITHUB", 'ocade-fusion-system'); // Nom de l'organisation GitHub
-define("DEPOT_GITHUB", 'ocade-blocks'); // Nom du dépôt GitHub
-define('OCADE_IS_THEME', false); // True = Thème / False = Plugin
-define('OCADE_IS_CHILD_THEME', false); // Est le thème enfant ?
-define('OCADE_PLUGIN_SLUG', 'ocade-blocks'); // Slug du plugin
-
-// Partie dynamique
-define('OCADE_PLUGIN_REPO', 'https://github.com/' . ORGANISATION_GITHUB . '/' . DEPOT_GITHUB);
-define('OCADE_VERSION_URL', OCADE_PLUGIN_REPO . '/releases/latest/download/version.txt');
-define('OCADE_ZIP_URL', OCADE_PLUGIN_REPO . '/releases/latest/download/' . DEPOT_GITHUB . '.zip');
-define('OCADE_REMOTE_VERSION', DEPOT_GITHUB . '_remote_version');
-define('OCADE_ICON_SVG_URL', 'https://raw.githubusercontent.com/' . ORGANISATION_GITHUB . '/' . DEPOT_GITHUB . '/master/assets/icons/icon.svg');
-define('OCADE_ICON_1X_URL',  'https://raw.githubusercontent.com/' . ORGANISATION_GITHUB . '/' . DEPOT_GITHUB . '/master/assets/icons/icon-1x.png');
-define('OCADE_ICON_2X_URL',  'https://raw.githubusercontent.com/' . ORGANISATION_GITHUB . '/' . DEPOT_GITHUB . '/master/assets/icons/icon-2x.png');
-define('OCADE_ICON_3X_URL',  'https://raw.githubusercontent.com/' . ORGANISATION_GITHUB . '/' . DEPOT_GITHUB . '/master/assets/icons/icon-3x.png');
-define('OCADE_ICON_4X_URL',  'https://raw.githubusercontent.com/' . ORGANISATION_GITHUB . '/' . DEPOT_GITHUB . '/master/assets/icons/icon-4x.png');
-define('OCADE_ICON_5X_URL',  'https://raw.githubusercontent.com/' . ORGANISATION_GITHUB . '/' . DEPOT_GITHUB . '/master/assets/icons/icon-5x.png');
-
-switch (true) {
-  case OCADE_IS_THEME:
-    require_once get_template_directory() . '/inc/theme-updater.php';
-    break;
-  case OCADE_IS_CHILD_THEME:
-    require_once get_stylesheet_directory() . '/inc/theme-updater.php';
-    break;
-  default:
-    require_once plugin_dir_path(__FILE__) . 'inc/plugin-updater.php';
-    break;
-}
-
+require_once plugin_dir_path(__FILE__) . 'inc/plugin-updater.php';
 
 /******************* DEBUT DU PLUGIN *******************************/
 /** Register PHP Loader  */
