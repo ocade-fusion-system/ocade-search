73186609dc4740188dbf006d3049d320383eb0e8
Valentin Charrier <valentin.charrier@hotmail.fr>
2025-03-17 21:45:35 +0100

    CP GIT

--- inc/plugin-updater.php
+++ inc/plugin-updater.php
@@ -9,12 +9,12 @@ add_filter('site_transient_update_plugins', function ($transient) {
   $organisation_github = 'ocade-fusion-system'; // ORGANISATION_GITHUB
   $plugin_slug = 'ocade-blocks'; // DEPOT_GITHUB
   $plugin_file = WP_PLUGIN_DIR . '/' . $plugin_slug . '/' . $plugin_slug . '.php';
-  $version_url = 'https://raw.githubusercontent.com/' . $organisation_github . '/' . $plugin_slug . '/master/version.txt';
-  $repo_url = 'https://github.com/' . $organisation_github . '/' . $plugin_slug;
+  $github_api_url = "https://api.github.com/repos/$organisation_github/$plugin_slug/releases/latest";
+  $repo_url = "https://github.com/$organisation_github/$plugin_slug";
   $zip_url = $repo_url . '/releases/latest/download/' . $plugin_slug . '.zip';
 
   // Icônes dynamiques
-  $icon_base_url = 'https://raw.githubusercontent.com/' . $organisation_github . '/' . $plugin_slug . '/master/assets/icons/';
+  $icon_base_url = $repo_url . '/master/assets/icons/';
   $icons = [
     'svg'  => $icon_base_url . 'icon.svg',
     '1x'   => $icon_base_url . 'icon-1x.png',
@@ -33,18 +33,50 @@ add_filter('site_transient_update_plugins', function ($transient) {
 
   // Récupérer la version distante (mise en cache pour éviter les requêtes répétées)
   $remote_version = get_transient($plugin_slug . '_remote_version');
-  if (false === $remote_version || is_null($remote_version)) {
-    $response = wp_remote_get($version_url);
-    if (is_wp_error($response)) return $transient;
+  if (!$remote_version) {
+    // Récupération des assets de la dernière release via l'API GitHub
+    $response = wp_remote_get($github_api_url, [
+      'headers' => [
+        'User-Agent' => 'WordPress' // GitHub requiert un User-Agent personnalisé
+      ]
+    ]);
 
-    $remote_version = trim(wp_remote_retrieve_body($response));
-    $remote_version = preg_replace('/[^0-9.]/', '', $remote_version); // Nettoyage
-    $remote_version = trim($remote_version);
-    set_transient($plugin_slug . '_remote_version', $remote_version, 6 * HOUR_IN_SECONDS);
+    if (is_wp_error($response)) {
+      error_log('Erreur lors de la récupération de la release GitHub : ' . $response->get_error_message());
+      return $transient;
+    }
+
+    $body = json_decode(wp_remote_retrieve_body($response), true);
+
+    if (!empty($body['assets'])) {
+      foreach ($body['assets'] as $asset) {
+        if ($asset['name'] === 'version.txt') {
+          $version_file_url = $asset['browser_download_url'];
+          break;
+        }
+      }
+    }
+
+    if (!empty($version_file_url)) {
+      $version_response = wp_remote_get($version_file_url);
+
+      if (!is_wp_error($version_response)) {
+        $remote_version = trim(wp_remote_retrieve_body($version_response));
+        $remote_version = preg_replace('/[^0-9.]/', '', $remote_version);
+
+        if (!empty($remote_version)) {
+          set_transient($plugin_slug . '_remote_version', $remote_version, 6 * HOUR_IN_SECONDS);
+        } else {
+          error_log('La version récupérée depuis les assets est vide.');
+        }
+      }
+    } else {
+      error_log('Aucun fichier version.txt trouvé dans les assets de la dernière release.');
+    }
   }
 
   // Comparaison des versions
-  if (version_compare($remote_version, $current_version, '>')) {
+  if (!empty($remote_version) && version_compare($remote_version, $current_version, '>')) {
     $plugin_basename = plugin_basename($plugin_file);
 
     $transient->response[$plugin_basename] = (object) [
