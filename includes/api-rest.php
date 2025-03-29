faf7ad3c398d5ce9d26df1db7f377fbf4dc33be6
Valentin Charrier <valentin.charrier@hotmail.fr>
2025-03-29 16:20:41 +0100

    CP GIT

--- includes/api-rest.php
+++ includes/api-rest.php
@@ -1,18 +1,14 @@
-<?php 
+<?php
 
 /** Renseigne la route REST pour les ngrammes. */
-add_action('rest_api_init', function () {
-  register_rest_field('post', 'ngrammes', [
-    'get_callback'    => function ($post_arr) {
-      return get_post_meta($post_arr['id'], '_ngrammes', true);
+add_action('init', function () {
+  register_meta('post', '_ngrammes', [
+    'type'              => 'string',
+    'single'            => true,
+    'sanitize_callback' => 'sanitize_text_field',
+    'show_in_rest'      => true, // C'est cette ligne qui est indispensable
+    'auth_callback'     => function () {
+      return current_user_can('edit_posts');
     },
-    'update_callback' => function ($value, $post_obj) {
-      update_post_meta($post_obj->ID, '_ngrammes', sanitize_text_field($value));
-    },
-    'schema'          => [
-      'description' => 'Ngrammes pour le moteur de recherche',
-      'type'        => 'string',
-      'context'     => ['view', 'edit', 'embed'],
-    ],
   ]);
 });
