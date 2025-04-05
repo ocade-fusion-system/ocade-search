3012229142db6bc0f66af84353a90ead1581716b
Valentin Charrier <valentin.charrier@hotmail.fr>
2025-04-05 15:21:56 +0200

    CP GIT

--- ocade-search.php
+++ ocade-search.php
@@ -10,7 +10,10 @@
  * Version: 1.0.37
  */
 
- if (is_admin()) require_once plugin_dir_path(__FILE__) . 'inc/plugin-updater.php'; // Mettre à jour le plugin depuis un dépôt Git
+add_action('admin_init', function () {
+  if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], 'update-core.php') !== false) require_once plugin_dir_path(__FILE__) . 'inc/plugin-updater.php';
+});
+
 require_once plugin_dir_path(__FILE__) . 'includes/admin-meta-box.php'; // Ajouter un champ personnalisé pour les ngrammes dans les articles
 require_once plugin_dir_path(__FILE__) . 'includes/api-rest.php'; // Permettre l'accès aux ngrammes via l'API REST
 require_once plugin_dir_path(__FILE__) . 'includes/ajax-indexation.php'; // Permettre de regénérer l'index de recherche
